d1f1e6aba503049bcb8bb5f2c4c9a63e8e0eb6d3
MikBar2501 <45071328+MikBar2501@users.noreply.github.com>
2019-04-08 13:33:20 +0200

    poprawki

--- Assets/SplineController.cs
+++ Assets/SplineController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SplineController : MonoBehaviour {
@@ -32,6 +33,10 @@ public class SplineController : MonoBehaviour {
 
 		if(Input.GetKeyDown(KeyCode.I)) {
 			HideInstruction();
+		}
+		
+		if(Input.GetKeyDown(KeyCode.Escape)) {
+			SceneManager.LoadScene("SplineScene02");
 		}
 	}
 
